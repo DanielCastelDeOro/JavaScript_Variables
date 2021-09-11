7aa7102d0631e6bc85cfc7cbd6009ecede76b32f
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-09-11 09:48:00 -0600

    js commit

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
+let firstName = "Daniel";
+let lastName = "Castel De Oro";
+var age = 35;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 
+let fullNametemp = `${firstName} ${lastName}`;
 // Exercise 3
+let city = `Highlands Ranch`;
+let passtime = `Being with family`;
+
+let myStory = `Hell my name is ${fullName}, I'm ${age}, and I love ${passtime}.`;
+console.log(myStory);
+
+for (let i = 0; i < age.length; i++) {
+  
+  console.log(i);
+}
+
+if (age < 3) {
+  console.log("hello");
+}
+else{
+  console.log("no");
+}
